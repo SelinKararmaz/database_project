271189b5d38b41fc68cb917e092a2467d7e0553c
Selin Kararmaz <77620963+SelinKararmaz@users.noreply.github.com>
2022-04-02 14:23:35 -0400

    Create projectDatabase.sql

---
+++ projectDatabase.sql
@@ -0,0 +1,22 @@
+SET SEARCH_PATH = 'project'
+
+CREATE TABLE IF NOT EXISTS "DCMS"."Employee"
+(
+    "ID" integer NOT NULL,
+    "firstName" VARCHAR(255) NOT NULL,
+    "lastName" VARCHAR(255) NOT NULL,
+    "houseNo" integer,
+    street VARCHAR(255),
+    city VARCHAR(255),
+    province VARCHAR(255),
+    "postalCode" VARCHAR(255),
+    role VARCHAR(255),
+    "employeeType" VARCHAR(255),
+    salary integer,
+    "SSN" VARCHAR(255),
+    "emailAddress" VARCHAR(255),
+    "DOB" DATE,
+    "phone number" VARCHAR(255),
+    age integer,
+    CONSTRAINT "Employee_pkey" PRIMARY KEY ("ID")
+)
